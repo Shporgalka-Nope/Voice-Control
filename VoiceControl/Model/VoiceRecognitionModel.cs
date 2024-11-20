934389bdd26db4e7d9466313fa39929c86d8e81e
ReAA <reaa@mer.ci.nsu.ru>
2024-11-20 10:19:37 +0700

    Preparations, slight main window edits

---
+++ VoiceControl/Model/VoiceRecognitionModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Windows.Controls;
+
+namespace VoiceControl.Model
+{
+    internal class VoiceRecognitionModel
+    {
+        public GrammarBuilder gb
+        {
+            get { return _gb; }
+            set { _gb = value; }
+        }
+        private GrammarBuilder _gb;
+        
+        private SpeechRecognitionEngine sr;
+
+        public VoiceRecognitionModel()
+        {
+            sr.SetInputToDefaultAudioDevice();
+            gb.Append(new Choices("Left", "Right", "Up", "Down"));
+            sr.UnloadAllGrammars();
+
+        }
+
+        public void AppendNewChoices(List<string> choices)
+        {
+            //TO-DO
+        }
+    }
+}
